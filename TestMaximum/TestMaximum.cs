414c525437d685f6d33f11949eb4884eebee4cf6
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2023-01-19 02:51:26 +0530

    [Arun] Refactored code to generic class and method

--- TestMaximum/TestMaximum.cs
+++ TestMaximum/TestMaximum.cs
@@ -8,123 +8,134 @@ namespace TestMaximum
         // test case 1 : max value at first position
         public static void TestCase1Integer()
         {
-            int result = TestMaximum.Comparer(10, 5, 6);
-            Console.WriteLine("\nMaximum value is " + result);
+            TestMaximum<int>.Comparer(10, 5, 6);
         }
 
         //test case 2: max value at second position
         public static void TestCase2Integer()
         {
-            int result = TestMaximum.Comparer(10, 15, 6);
-            Console.WriteLine("\nMaximum value is " + result);
+            TestMaximum<int>.Comparer(10, 15, 6);
         }
 
         //test case 3: max value at third position
         public static void TestCase3Integer()
         {
-            int result = TestMaximum.Comparer(10, 5, 16);
-            Console.WriteLine("\nMaximum value is " + result);
+            TestMaximum<int>.Comparer(10, 5, 16);
         }
 
         //Test cases for float values
         //test case 1: max value at first position
         public static void TestCase1Float()
         {
-            float result = TestMaximum.Comparer(21.5f, 8.5f, 15.7f);
-            Console.WriteLine("\nMaximum value is " + result);
+            TestMaximum<float>.Comparer(21.5f, 8.5f, 15.7f);
         }
         //test case 2: max value at second position
         public static void TestCase2Float()
         {
-            float result = TestMaximum.Comparer(2.6f, 15.4f, 1.5f);
-            Console.WriteLine("\nMaximum value is " + result);
+            TestMaximum<float>.Comparer(2.6f, 15.4f, 1.5f);
         }
         //test case 3: max value at third position
         public static void TestCase3Float()
         {
-            float result = TestMaximum.Comparer(2.6f, 8.5f, 15.7f);
-            Console.WriteLine("\nMaximum value is " + result);
+            TestMaximum<float>.Comparer(2.6f, 8.5f, 15.7f);
         }
 
         //Test cases for String values
         //test case 1: max value at first position
         public static void TestCase1String()
         {
-            String result = TestMaximum.Comparer("Peach", "Apple", "Banana");
-            Console.WriteLine("\nMaximum value is " + result);
+            TestMaximum<string>.Comparer("Peach", "Apple", "Banana");
         }
         //test case 2: max value at second position
         public static void TestCase2String()
         {
-            String result = TestMaximum.Comparer("Lion", "Zebra", "Cat");
-            Console.WriteLine("\nMaximum value is " + result);
+            TestMaximum<string>.Comparer("Lion", "Zebra", "Cat");
         }
         //test case 3: max value at third position
         public static void TestCase3String()
         {
-            String result = TestMaximum.Comparer("Blue", "Red", "Xerox");
-            Console.WriteLine("\nMaximum value is " + result);
+            TestMaximum<string>.Comparer("Blue", "Red", "Xerox");
         }
     }
-    internal class TestMaximum
+    internal class TestMaximum <T> //refactored as a generic class
     {
-        public static int Comparer(int first, int second , int third) // UC1 Comparing 3 integer values
+        //public static int Comparer(int first, int second , int third) // UC1 Comparing 3 integer values
+        //{
+        //    if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+        //    {
+        //        return first;
+        //    }
+        //    else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+        //    {
+        //        return second;
+        //    }
+        //    else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+        //    {
+        //        return third;
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("First,second and third are same");
+        //        return 0;
+        //    }
+        //}
+
+        //public static float Comparer(float first, float second , float third) // UC2 Comparing 3 float values
+        //{
+        //    if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+        //    {
+        //        return first;
+        //    }
+        //    else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+        //    {
+        //        return second;
+        //    }
+        //    else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+        //    {
+        //        return third;
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("First,second and third are same");
+        //        return 0;
+        //    }
+        //}
+
+        //public static string Comparer(string first, string second, string third) // UC3 Comparing 3 string values
+        //{
+        //    if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+        //    {
+        //        return first;
+        //    }
+        //    else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+        //    {
+        //        return second;
+        //    }
+        //    else
+        //    {
+        //        return third;
+        //    }
+        //}
+
+        public static void Comparer(T first, T second, T third) //Refactored as a generic method
         {
-            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            dynamic d1 = first;
+            dynamic d2 = second;
+            dynamic d3 = third;
+            if (d1.CompareTo(d2) > 0 && d1.CompareTo(d3) > 0)
             {
-                return first;
+                Console.WriteLine("Max value is : " + first);
             }
-            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            else if (d2.CompareTo(d1) > 0 && d2.CompareTo(d3) > 0)
             {
-                return second;
-            }
-            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
-            {
-                return third;
+                Console.WriteLine("Max value is : " + second);
             }
             else
             {
-                Console.WriteLine("First,second and third are same");
-                return 0;
+                Console.WriteLine("Max value is : " + third);
             }
         }
 
-        public static float Comparer(float first, float second , float third) // UC2 Comparing 3 float values
-        {
-            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
-            {
-                return first;
-            }
-            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
-            {
-                return second;
-            }
-            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
-            {
-                return third;
-            }
-            else
-            {
-                Console.WriteLine("First,second and third are same");
-                return 0;
-            }
-        }
-
-        public static string Comparer(string first, string second, string third) // UC3 Comparing 3 string values
-        {
-            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
-            {
-                return first;
-            }
-            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
-            {
-                return second;
-            }
-            else
-            {
-                return third;
-            }
-        }
 
 
         static void Main(string[] args)
